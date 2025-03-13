f2194991ca43b1cb75fcf8436070a2914f30b6a2
Weslley Costa <weslleycosta.ti@gmail.com>
2025-03-12 21:03:48 -0300

    ci: configuração pastas static e media

--- core/settings.py
+++ core/settings.py
@@ -126,7 +126,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL ='/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from produtos.views import ProdutoListView
+from produtos.views import ProdutoListView, ProdutoCreateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', ProdutoListView.as_view(), name='produtos-lista' )
+    path('', ProdutoListView.as_view(), name='produtos-lista' ),
+    path('produto/add/', ProdutoCreateView.as_view(), name='produto-create')
+    
 ]
