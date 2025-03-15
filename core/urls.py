92b441f8d561d6b0445c6c9d38052825bac05d3a
Weslley Costa <weslleycosta.ti@gmail.com>
2025-03-14 21:08:37 -0300

    style(produtos.html) e urls

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from produtos.views import ProdutoListView, ProdutoCreateView
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', ProdutoListView.as_view(), name='produtos-lista' ),
-    path('produto/add/', ProdutoCreateView.as_view(), name='produto-create')
-    
+    path('produto/add/', ProdutoCreateView.as_view(), name='produto-create'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
