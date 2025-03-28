f373ffc114ce3421cfe05d2499fec0de0bc7209b
Weslley Costa <weslleycosta.ti@gmail.com>
2025-03-27 21:13:44 -0300

    fix(produto): template e imagens

--- core/settings.py
+++ core/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = []
 # )
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -131,8 +130,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL ='/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- produtos/models.py
+++ produtos/models.py
@@ -36,7 +36,7 @@ class Produto(models.Model):
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT )
     fornecedor= models.ForeignKey(Fornecedor, on_delete=models.PROTECT )
     usuario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    imagem = models.ImageField(null=True, blank=True, default='/images/placeholder.png')
+    imagem = models.ImageField(null=True, blank=True, upload_to='imagens')
     marca = models.CharField(max_length=200, null=True, blank=True)
     avaliacao = models.DecimalField(max_digits=10,decimal_places=2, null=True, blank=True)
     criadoEm = models.DateTimeField(auto_now_add=True)
